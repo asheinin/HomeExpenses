55fa4fe3f90f0143818aabb872a591ad5debe6e1
asheinin <alexander.sheinin@gmail.com>
2026-02-20 14:12:16 -0500

    fix drop down

--- src/open.js
+++ src/open.js
@@ -326,7 +326,7 @@ function validateType(row, col) {
     var range = sheet.getRange(row, myNumbers.expenseTypeColumn);
     console.log(myNumbers.expenseTypeColumn + " " + col);
     var summaryValues = summarySheet.getRange(myNumbers.expenseFirstRow, myNumbers.expenseTypeColumn, lastFilledRow - myNumbers.expenseFirstRow + 1, 1).getValues().flat();
-    console.log("drop down list: myNumbers.expenseFirstRow + " " + myNumbers.expenseTypeColumn + " " + lastFilledRow - myNumbers.expenseFirstRow + " " + 1);
+    console.log("drop down list: "myNumbers.expenseFirstRow + " " + myNumbers.expenseTypeColumn + " " + lastFilledRow - myNumbers.expenseFirstRow + " " + 1);
 
     if (firstEmptyIndex !== -1) {
       summaryValues = summaryValues.slice(0, firstEmptyIndex);
