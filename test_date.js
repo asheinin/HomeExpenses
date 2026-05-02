cdc3f4dcb6b230afd8019eca7ccef4359365b8cc
asheinin <alexander.sheinin@gmail.com>
2026-05-02 12:37:17 -0400

    tax receipt fixes#5

--- test_date.js
+++
@@ -1,3 +0,0 @@
-var d = new Date("2026-01-01T00:00:00");
-console.log(d.toISOString());
-console.log(d.getMonth());
--- test_parse.js
+++
@@ -1,5 +0,0 @@
-process.env.TZ = 'America/New_York';
-var startDate = new Date("2026-01-01T00:00:00");
-console.log("Date:", startDate.toString());
-console.log("Month:", startDate.getMonth());
-console.log("Year:", startDate.getFullYear());
